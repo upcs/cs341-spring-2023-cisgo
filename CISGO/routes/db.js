6d99dc278e121bb1c7efe075900aa3e1b2ef24df
Carter Rhoades <rhoades26@up.edu>
2023-03-23 13:14:54 -0600

    for merge

--- CISGO/routes/db.js
+++ CISGO/routes/db.js
@@ -2,19 +2,20 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     var con = mysql.createConnection({
-        host: "",
-        user: "",
-        password: "",
-        database: ""
+        host: "10.13.6.44",
+        user: "cisgouser",
+        password: "cisgo",
+        database: "cisgo"
     })
     con.connect(function(err) {
         if(err) throw err;
         //console.log("Connected");
-        con.query("SELECT * FROM csvjson", function (err, result, fields) {
+        console.log(req.body.query);
+        con.query(req.body.query, function (err, result, fields) {
         if (err) throw err;
+        console.log(result);
         res.send(result);
         })
     })
