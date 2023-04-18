854984798977872cfbf1def371781f654aeb8743
Marcus Rison <rison25@up.edu>
2023-04-18 13:14:57 -0700

    Fixed sidebar

--- CISGO/public/javascripts/chart.js
+++ CISGO/public/javascripts/chart.js
@@ -1,9 +1,14 @@
 
 
 function openNav() {
-	document.getElementById("mySidebar").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
-}
+	var sidebar = document.getElementById("mySidebar");
+	if (sidebar.style.width === "250px") {
+	  closeNav();
+	} else {
+	  sidebar.style.width = "250px";
+	  document.getElementById("main").style.marginLeft = "250px";
+	}
+  }
 
 function closeNav() {
 	document.getElementById("mySidebar").style.width = "0";
