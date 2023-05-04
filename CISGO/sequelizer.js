3e1f7885647e2863305a89403b2913851a06bfb3
rhoades26 <120342545+rhoades26@users.noreply.github.com>
2023-05-04 10:34:18 -0600

    update

--- CISGO/sequelizer.js
+++ CISGO/sequelizer.js
@@ -1,7 +1,7 @@
 
 const { Sequelize } = require('sequelize')
 
-
+//initializes sequelize connection to database
 const sequelize = new Sequelize('interactivemap', 'cisgouser@cisgodb', 'cisgo', {
   host: 'cisgodb.mariadb.database.azure.com',
   dialect: 'mariadb'
