5c745f536bfb32f1c4bf93414b1cd3c5600cb702
Marcus Rison <rison25@up.edu>
2023-05-03 20:48:14 -0700

    bruh

--- CISGO/public/javascripts/chart.js
+++ CISGO/public/javascripts/chart.js
@@ -1,175 +1,21 @@
 function openNav() {
-	var sidebar = document.getElementById("mySidebar");
-	if (sidebar.style.width === "250px") {
-	  closeNav();
-	} else {
-	  sidebar.style.width = "250px";
-	  document.getElementById("main").style.marginLeft = "250px";
-	}
-  }
+    var sidebar = document.getElementById("mySidebar");
+    if (sidebar.style.width === "250px") {
+        closeNav();
+    } else {
+        sidebar.style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+}
 
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-/*
-am5.ready(function () {
-
-    var newPoint = new Object();;
-    var pointData = [];
-    retrieveCountries().then(array => {
-        console.log(array);
-        array.forEach(function (array, index) {
-
-            console.log(array);
-            newPoint = findCoordsByName(array);
-            if (newPoint) {
-                pointData.push({
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [newPoint[1], newPoint[2]]
-                    },
-                    "name": array,
-                    "what": "gja",
-                    "org": "U of New York",
-                    "purpose": "Build Trees"
-                });
-
-                //String value = (String) jsonObject. get("key_name"); 
-                console.log("Name: " + JSON.stringify(pointData));
-            }
-        });
-        // Create root element
-        // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-        var root = am5.Root.new("chartdiv");
-
-
-        // Set themes
-        // https://www.amcharts.com/docs/v5/concepts/themes/
-        root.setThemes([
-            am5themes_Animated.new(root)
-        ]);
-
-
-        // Create the map chart
-        // https://www.amcharts.com/docs/v5/charts/map-chart/
-        var chart = root.container.children.push(am5map.MapChart.new(root, {
-            panX: "rotateX",
-            // panY: "rotateY",
-            // projection: am5map.geoOrthographic()
-            projection: am5map.geoOrthographic()
-        }));
-
-
-        // Create series for background fill
-        // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/#Background_polygon
-        var backgroundSeries = chart.series.push(
-            am5map.MapPolygonSeries.new(root, {})
-        );
-        backgroundSeries.mapPolygons.template.setAll({
-            fill: am5.color(0x146DFF),
-            fillOpacity: 0.2,
-            strokeOpacity: 0
-        });
-        backgroundSeries.data.push({
-            geometry:
-                am5map.getGeoRectangle(90, 180, -90, -180)
-        });
-
-
-        // Create main polygon series for countries
-        // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/
-        var polygonSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
-            geoJSON: am5geodata_worldLow
-        }));
-        polygonSeries.mapPolygons.template.setAll({
-            fill: am5.color(0x146DFF),
-            fillOpacity: 0.15,
-            strokeWidth: 0.25,
-            stroke: root.interfaceColors.get("background")
-        });
-
-        // change color on hover for countries 
-        polygonSeries.mapPolygons.template.states.create("hover", {
-            fill: am5.color(0xF90349) //F90349
-        });
-
-        // Create point series
-        var pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
-
-        var testArray = [{
-            "geometry": {
-                "type": "Point",
-                "coordinates": [-73.778137, 40.641312]
-            },
-            "name": "New York",
-            "what": "gja",
-            "org": "U of New York",
-            "purpose": "Build Trees"
-        }, {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [-0.454296, 51.470020]
-            },
-            "name": "[bold]Study Abroad\n" + "[unbold]London, UK\n" + "2001-Present\n" + "[underline]more",
-            "what": "duple tuple",
-            "org": "University of London",
-            "purpose": "Build boats"
-
-        }, {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [116.597504, 40.072498],
-            },
-            "name": "Beijing",
-            "what": "Big company",
-            "org": "U of China",
-            "purpose": "Build cars"
-
-        }];
-        console.log(JSON.stringify(testArray));
-        console.log(JSON.stringify(pointData));
-        pointSeries.data.setAll(pointData);
-
-
-        pointSeries.bullets.push(function () {
-            var circle = am5.Circle.new(root, {
-                radius: 5,
-                fill: am5.color(0xffba00),
-                tooltipText: "{name}",
-            });
-
-            var ttt = circle.tooltipText;
-
-            circle.events.on("click", function (ev) {
-                var markerData = ev.target.dataItem.dataContext;
-                var markerDetails = "Name: " + markerData.name + ", What: " + markerData.what + ", Org: " + markerData.org + ", Purpose: " + markerData.purpose;
-                // Get the sidebar links
-                var sidebarLinks = document.getElementById("mySidebar");
-
-                sidebarLinks.textContent = markerDetails;
-                sidebarLinks.style.fontSize = "16px";
-                sidebarLinks.style.color = "white";
-
-                ttt.setText(markerData);
-            });
-
-            return am5.Bullet.new(root, {
-                sprite: circle
-            });
-        });
-
-
-        // Make stuff animate on load
-        chart.appear(1000, 100);
-    });
-    console.log("PointData after resolve: " + JSON.stringify(pointData));
 
 
 
-}); // end am5.ready()
-*/
 
 am5.ready(function () {
 
@@ -284,6 +130,7 @@ am5.ready(function () {
 
             circle.events.on("click", function (ev) {
                 var markerData = ev.target.dataItem.dataContext;
+
                 var markerDetails =
                     "Country: " + markerData.country + "\n" +
                     "Start Date: " + markerData.start_date + "\n" +
@@ -296,6 +143,10 @@ am5.ready(function () {
                     "Link: " + markerData.link + "\n" +
                     "Student Involvement: " + markerData.stu_inv + "\n" +
                     "Student Role: " + markerData.stu_role + "\n";  
+
+                var markerDetails = markerData.country + markerData.start_date + markerData.end_date + markerData.finished + markerData.S_U_D + markerData.eng_type + markerData.time + markerData.desc + markerData.link + markerData.stu_inv + markerData.stu_role;
+
+
                 // Get the sidebar links
                 var sidebarLinks = document.getElementById("mySidebar");
 
@@ -315,4 +166,4 @@ am5.ready(function () {
         // Make stuff animate on load
         chart.appear(1000, 100);
     });
-    }); // end am5.ready()
+}); // end am5.ready()
--- CISGO/sequelizer.js
+++ CISGO/sequelizer.js
@@ -1,8 +1,12 @@
+
 const { Sequelize } = require('sequelize')
+
 
 const sequelize = new Sequelize('interactivemap', 'cisgouser@cisgodb', 'cisgo', {
   host: 'cisgodb.mariadb.database.azure.com',
   dialect: 'mariadb'
 })
+
+
 
 module.exports = sequelize
